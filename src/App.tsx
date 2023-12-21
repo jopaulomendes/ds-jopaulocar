6cabae0a5d8ecf865fa28e291868bbdbf3411e7f
João Paulo <jp_cbc@hotmail.com>
2023-12-21 11:51:58 -0300

    iniciando projeto Carros Top e finalizando Navbar

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './assets/styles/custom.scss'
+import './App.css';
+import Navbar from 'components/Navbar';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <h3>Olá, mundo cruel</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import 'bootstrap/dist/css/bootstrap.css'; importa o bootrstrap para o projeto
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
